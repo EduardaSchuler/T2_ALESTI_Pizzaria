e7ae4a479a062dac97e2fb4cde1398d3ef75dfb1
Salette <kimsalette@gmail.com>
2024-06-29 15:39:36 -0300

    criei os csv de saida (ainda nao implementei a saida correta)

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -16,15 +16,18 @@ public class App {
     private Scanner entrada;
     private FilaPedidosDinamica filaPedidosDinamica;
     private Pizzaria pizzaria;
-
+    private PrintStream saidaGeral;
+    private PrintStream situacaoFilaSaida;
+    private PrintStream caminhamentoCentralSaida;
 
     public App(){
         try {
             BufferedReader streamEntrada = new BufferedReader(new FileReader("pedidos_pizza_1000.csv"));
             entrada = new Scanner(streamEntrada);
             entrada.useDelimiter("[;\r]");
-            PrintStream streamSaida = new PrintStream(new File("saida.csv"), StandardCharsets.UTF_8);
-            System.setOut(streamSaida);
+            saidaGeral = new PrintStream(new File("saida.csv"), StandardCharsets.UTF_8);
+            situacaoFilaSaida = new PrintStream(new File("situacao_fila.csv"), StandardCharsets.UTF_8);
+            caminhamentoCentralSaida = new PrintStream(new File("caminhamento_central.csv"), StandardCharsets.UTF_8);
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -32,10 +35,8 @@ public class App {
         pizzaria = new Pizzaria();
     }
 
-
-
     public void executa(){
-        new JanelaPedido();
+        //new JanelaPedido();
         recebePedido();
     }
 
@@ -45,16 +46,26 @@ public class App {
         int instante;
         int tempoPreparo;
         Pedido pedido;
-        while(entrada.hasNextLine()){
+        while (entrada.hasNextLine()) {
             codigo = entrada.nextInt();
             saborPizza = entrada.next();
             instante = entrada.nextInt();
             tempoPreparo = entrada.nextInt();
             pedido = new Pedido(codigo, saborPizza, instante, tempoPreparo);
             pizzaria.adicionarPedido(pedido);
-            System.out.println(pedido.toString());
+            saidaGeral.println(pedido.toString());
             entrada.nextLine();
         }
 
+        situacaoFilaSaida.println("implementar");
+
+        caminhamentoCentralSaida.println("implementar");
+    }
+
+    private void closeStreams() {
+        saidaGeral.close();
+        situacaoFilaSaida.close();
+        caminhamentoCentralSaida.close();
     }
 }
+
