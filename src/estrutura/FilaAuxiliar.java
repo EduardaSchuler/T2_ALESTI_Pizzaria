05d2842645e6619312a150861b24220a98d99a7c
ArthurRoyerMombach <arthur.mombach@edu.pucrs.br>
2024-07-01 01:11:29 -0300

    Criação de uma fila encadeada auxiliar para guardar os objetos.
    Revisão de todas outras classes para verificar se a execução estava apropriada. Todos os métodos que estão funcionando corretamente estão marcados com o comentário "Certo!".

---
+++ src/estrutura/FilaAuxiliar.java
@@ -0,0 +1,40 @@
+package estrutura;
+
+import gerenciamento.Pedido;
+
+// Nesta fila guardaremos todos os objetos que posteriormente serão realocados para a FilaPedidosDinamica
+public class FilaAuxiliar {
+    private Nodo inicio;
+    private Nodo fim;
+    private int tamanho;
+
+    public FilaAuxiliar() {
+        inicio = null;
+        fim = null;
+        tamanho = 0;
+    }
+
+    // Método para adicionar um pedido na fila auxiliar.
+    public void enfileirar(Pedido pedido) {
+        Nodo n = new Nodo(pedido);
+        if (tamanho == 0) {
+            inicio = n;
+            fim = n;
+        } else {
+            fim.setProximo(n);
+            fim = n;
+        }
+        tamanho++;
+    }
+
+    // Método para tirar um pedido da fila auxiliar.
+    public Pedido desenfileirar() {
+        if (tamanho > 0) {
+            if (inicio.getProximo() != null) {
+                inicio = inicio.getProximo();
+                tamanho--;
+            }
+        }
+        return null;
+    }
+}
